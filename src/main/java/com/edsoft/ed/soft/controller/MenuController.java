22cd5576d7e719c736f20470425d857e6d38e61d
Erdincdagli35 <erdinc96dagli35@gmail.com>
2025-07-06 12:08:26 +0300

    Product name and descriptions edited

--- src/main/java/com/edsoft/ed/soft/controller/MenuController.java
+++ src/main/java/com/edsoft/ed/soft/controller/MenuController.java
@@ -19,51 +19,49 @@ public class MenuController {
     @GetMapping
     public ResponseEntity<List<MenuItem>> getMenu() {
         List<MenuItem> menuItems = List.of(
-                new MenuItem("Mezeler", "Normal mezelerin hepsinin fiyatı aynıdır.", new BigDecimal("200")),
-                new MenuItem("Deniz Mah. Mezeler", "Deniz Mahsullü mezelerin hepsinin fiyatı aynıdır.", new BigDecimal("350")),
+                new MenuItem("Mezeler", "Tüm mezeler sabit fiyatlıdır.", new BigDecimal("200")),
+                new MenuItem("Deniz Mah. Mezeler", "Deniz ürünlü mezeler sabit fiyatlıdır.", new BigDecimal("350")),
 
-                new MenuItem("Kalamar", "Taze günlük kalamar", new BigDecimal("700")),
-                new MenuItem("Karides", "Tereyağında yapılmış karides", new BigDecimal("700")),
-                new MenuItem("Yaprak Ciğer", "Güveç yaprak ciğer", new BigDecimal("600")),
-                new MenuItem("Kokoreç", "", new BigDecimal("600")),
-                new MenuItem("Sigara Böreği", "", new BigDecimal("200")),
-                new MenuItem("Paçanga", "Tanesinin fiyatıdır", new BigDecimal("200")),
+                new MenuItem("Kalamar", "Günlük taze kalamar tava.", new BigDecimal("700")),
+                new MenuItem("Karides", "Tereyağında sotelenmiş karides.", new BigDecimal("700")),
+                new MenuItem("Yaprak Ciğer", "Izgara usulü yaprak ciğer.", new BigDecimal("600")),
+                new MenuItem("Kokoreç", "Özel baharatlı ızgara kokoreç.", new BigDecimal("600")),
+                new MenuItem("Sigara Böreği", "Kızartılmış klasik börek.", new BigDecimal("200")),
+                new MenuItem("Paçanga", "Birim fiyatıdır, içi et ve kaşar dolgulu.", new BigDecimal("200")),
 
-                new MenuItem("Yeşil Salata", "Normal=150, Duble=300", new BigDecimal("150")),
-                new MenuItem("Çoban Salata", "Normal=150, Duble=300", new BigDecimal("150")),
-                new MenuItem("Peynirli Salata", "Normal=250, Duble=450", new BigDecimal("250")),
+                new MenuItem("Yeşil Salata", "Normal: 150 TL, Duble: 300 TL", new BigDecimal("150")),
+                new MenuItem("Çoban Salata", "Normal: 150 TL, Duble: 300 TL", new BigDecimal("150")),
+                new MenuItem("Peynirli Salata", "Normal: 250 TL, Duble: 450 TL", new BigDecimal("250")),
 
-                new MenuItem("Antrikot", "", new BigDecimal("700")),
-                new MenuItem("Kuzu Lokum", "", new BigDecimal("600")),
-                new MenuItem("Kuzu Şiş", "", new BigDecimal("500")),
-                new MenuItem("Izgara Köfte", "", new BigDecimal("450")),
-                new MenuItem("Güveç Kavurma", "", new BigDecimal("850")),
-                new MenuItem("Pirzola", "", new BigDecimal("700")),
-                // Balık için fiyat boş, null yapabiliriz
-                new MenuItem("Balık", "", null),
+                new MenuItem("Antrikot", "Izgara antrikot, yumuşak ve sulu.", new BigDecimal("700")),
+                new MenuItem("Kuzu Lokum", "Lokum kıvamında kuzu eti.", new BigDecimal("600")),
+                new MenuItem("Kuzu Şiş", "Şişte kuzu et spesiyali.", new BigDecimal("500")),
+                new MenuItem("Izgara Köfte", "Ev yapımı dana köfte.", new BigDecimal("450")),
+                new MenuItem("Güveç Kavurma", "Toprak güveçte dana kavurma.", new BigDecimal("850")),
+                new MenuItem("Pirzola", "Izgarada pişirilmiş kuzu pirzola.", new BigDecimal("700")),
+                new MenuItem("Balık", "Fiyat sorunuz. Günlük taze balık.", null),
 
-                new MenuItem("Tuborg & Carslberg", "", new BigDecimal("200")),
-                new MenuItem("Sol", "", new BigDecimal("250")),
+                new MenuItem("Tuborg & Carlsberg", "Şişe bira çeşitleri.", new BigDecimal("200")),
+                new MenuItem("Sol", "Meksika menşeli bir ale bira.", new BigDecimal("250")),
 
-                new MenuItem("Rakı", "Göbek 20cl", new BigDecimal("1000")),
-                new MenuItem("Rakı", "Göbek 35cl", new BigDecimal("1400")),
-                new MenuItem("Rakı", "Göbek 50cl", new BigDecimal("1800")),
-                new MenuItem("Rakı", "Göbek 70cl", new BigDecimal("2200")),
-                new MenuItem("Rakı", "Göbek 100cl", new BigDecimal("2600")),
+                new MenuItem("Rakı", "Yeni Rakı Göbek 20 cl.", new BigDecimal("1000")),
+                new MenuItem("Rakı", "Yeni Rakı Göbek 35 cl.", new BigDecimal("1400")),
+                new MenuItem("Rakı", "Yeni Rakı Göbek 50 cl.", new BigDecimal("1800")),
+                new MenuItem("Rakı", "Yeni Rakı Göbek 70 cl.", new BigDecimal("2200")),
+                new MenuItem("Rakı", "Yeni Rakı Göbek 100 cl.", new BigDecimal("2600")),
 
-                new MenuItem("Rakı", "Mavi 20cl", new BigDecimal("900")),
-                new MenuItem("Rakı", "Mavi 35cl", new BigDecimal("1300")),
-                new MenuItem("Rakı", "Mavi 50cl", new BigDecimal("1600")),
-                new MenuItem("Rakı", "Mavi 70cl", new BigDecimal("2000")),
-                new MenuItem("Rakı", "Mavi 100cl", new BigDecimal("2300")),
+                new MenuItem("Rakı", "Yeni Rakı Mavi 20 cl.", new BigDecimal("900")),
+                new MenuItem("Rakı", "Yeni Rakı Mavi 35 cl.", new BigDecimal("1300")),
+                new MenuItem("Rakı", "Yeni Rakı Mavi 50 cl.", new BigDecimal("1600")),
+                new MenuItem("Rakı", "Yeni Rakı Mavi 70 cl.", new BigDecimal("2000")),
+                new MenuItem("Rakı", "Yeni Rakı Mavi 100 cl.", new BigDecimal("2300")),
 
-                new MenuItem("Rakı Tek", "", new BigDecimal("400")),
-                new MenuItem("Rakı Duble", "", new BigDecimal("600")),
+                new MenuItem("Rakı Tek", "Standart 4 cl servis.", new BigDecimal("400")),
+                new MenuItem("Rakı Duble", "Büyük boy 8 cl servis.", new BigDecimal("600")),
 
-                new MenuItem("Şarap Çeşitleri", "Red & Rose & White 70cl", new BigDecimal("15000")),
-                new MenuItem("Şarap Beyaz", "Carnet Savvignon White", new BigDecimal("17500")),
-
-                new MenuItem("Şarap Kadeh", "", new BigDecimal("400"))
+                new MenuItem("Şarap Çeşitleri", "Red, Rosé ve White – 70 cl.", new BigDecimal("15000")),
+                new MenuItem("Şarap Beyaz", "Cabernet Sauvignon White – 70 cl.", new BigDecimal("17500")),
+                new MenuItem("Şarap Kadeh", "Kadeh servis, kırmızı veya beyaz.", new BigDecimal("400"))
         );
 
         return ResponseEntity.ok(menuItems);
