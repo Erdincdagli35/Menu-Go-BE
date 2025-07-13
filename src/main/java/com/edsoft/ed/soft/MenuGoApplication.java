68c7814209774d12e880e2a10ee06827891cb368
Erdincdagli35 <erdinc96dagli35@gmail.com>
2025-07-13 12:11:48 +0300

    The project managed by Restaurant Menu

--- src/main/java/com/edsoft/ed/soft/MenuGoApplication.java
+++ src/main/java/com/edsoft/ed/soft/MenuGoApplication.java
@@ -1,15 +1,12 @@
 package com.edsoft.ed.soft;
 
-import com.edsoft.ed.soft.model.MenuItem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MenuGoApplication {
 
-	public static void main(String[] args) {
-		//MenuItem menuItem = new MenuItem("Espresso","Single Espresso",20.0);
-		SpringApplication.run(MenuGoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MenuGoApplication.class, args);
+    }
 }
